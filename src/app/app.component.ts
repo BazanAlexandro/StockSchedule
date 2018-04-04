d606726aaa7f796b956f0260b6f5ddb584d320d6
Alexander Bazan <alexb@cpart.co.il>
2018-04-04 22:34:06 +0300

    Simplifying app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { SchedulerMode } from './models/scheduler-mode';
-import { ShipRecord } from './models/ship-record';
-import * as moment from 'moment';
-import { CalendarSegment } from './models/calendar-segment';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +7,7 @@ import { CalendarSegment } from './models/calendar-segment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-
   mode: SchedulerMode = SchedulerMode.Week;
 
   SchedulerMode = SchedulerMode;
-
-  get isWeekMode() {
-    return this.mode === SchedulerMode.Week;
-  }
-
-  get isDayMode() {
-    return this.mode === SchedulerMode.Day;
-  }
-
-  toggleMode() {
-    this.mode = this.isWeekMode ? SchedulerMode.Day : SchedulerMode.Week;
-  }
 }
